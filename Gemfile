f15ec39f09d411b9ac0e9759257ebf0d3ef85049
priom <priom.galaxy@gmail.com>
2017-05-10 01:50:03 -0400

    1st test success

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
-
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
 group :development do
   gem 'sqlite3'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,26 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      flash[:success] = 'Article has been created'
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
