fea38b08e0059999b3cbe4d1a014cd340de63506
priom <priom.galaxy@gmail.com>
2017-05-13 22:32:08 -0400

    alternate index layout created

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -19,4 +19,23 @@ RSpec.feature 'Listing Articles' do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+
+  scenario 'A user has no articles' do
+    Article.delete_all
+
+    visit '/'
+
+    expect(page).not_to have_content(@article1.title)
+    expect(page).not_to have_content(@article1.body)
+
+    expect(page).not_to have_content(@article2.title)
+    expect(page).not_to have_content(@article2.body)
+
+    expect(page).not_to have_link(@article1.title)
+    expect(page).not_to have_link(@article2.title)
+
+    within('h1') do
+      expect(page).to have_content('No articles created yet!')
+    end
+  end
 end
